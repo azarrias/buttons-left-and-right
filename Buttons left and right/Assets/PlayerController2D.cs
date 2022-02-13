05d195e7e51bce67f3b1f5ee1be1a445bbf73b8e
azarrias <adolfo.zarrias@gmail.com>
2022-02-13 19:39:42 +0100

    Get Undo command working :D

--- Buttons left and right/Assets/PlayerController2D.cs
+++ Buttons left and right/Assets/PlayerController2D.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using Vector3 = UnityEngine.Vector3;
 
@@ -7,11 +8,13 @@ public class PlayerController2D : MonoBehaviour
 {
     [SerializeField] private LayerMask obstaclesLayerMask;
     private Direction selectedDirection;
-    private Direction[] movementHistory;
+    private readonly Stack<Direction> movementStack = new Stack<Direction>();
+    private bool undoLastMovement;
 
     public delegate void SelectDirection(Direction direction);
+
     public event SelectDirection OnSelectDirection;
-    
+
     // Music stuff, doesn't belong here
     private bool musicPlaying;
     [SerializeField] private AudioSource audioSource;
@@ -19,7 +22,7 @@ public class PlayerController2D : MonoBehaviour
     [SerializeField] private float musicBpm;
     private float movingRate;
     private int currentBeat;
-    
+
     public enum Direction
     {
         None,
@@ -39,25 +42,39 @@ public class PlayerController2D : MonoBehaviour
         selectedDirection = direction;
         OnSelectDirection?.Invoke(selectedDirection);
     }
-    
+
     private void Update()
     {
         HandleInput();
-        
+
         if (audioSource.time > movingRate * currentBeat + musicStartingOffset)
         {
             if (selectedDirection != Direction.None)
             {
                 TryMove();
             }
+            else if (undoLastMovement)
+            {
+                TryUndo();
+            }
+
             currentBeat++;
         }
     }
 
+    private Direction GetOppositeDirection(Direction direction) => direction switch
+    {
+        Direction.Right => Direction.Left,
+        Direction.Down => Direction.Up,
+        Direction.Left => Direction.Right,
+        Direction.Up => Direction.Down,
+        _ => throw new ArgumentOutOfRangeException(nameof(direction), $"Not expected direction value: {direction}")
+    };
+
     private void TryMove()
     {
         var currentPosition = transform.position;
-        var targetMovement = GetTargetDirection();
+        var targetMovement = GetTargetMovement(selectedDirection);
         var obstacles = Physics2D.Linecast(currentPosition, currentPosition + targetMovement, obstaclesLayerMask);
         if (!obstacles)
         {
@@ -65,10 +82,33 @@ public class PlayerController2D : MonoBehaviour
         }
     }
 
-    private void Move(Vector3 targetMovement)
+    private void Move(Vector3 movement)
     {
-        
-        StartCoroutine(SmoothMove(targetMovement));
+        movementStack.Push(selectedDirection);
+        StartCoroutine(SmoothMove(movement));
+    }
+    
+    private void TryUndo()
+    {
+        if (movementStack.Count == 0)
+        {
+            return;
+        }
+
+        var currentPosition = transform.position;
+        var direction = movementStack.Pop();
+        var oppositeDirection = GetOppositeDirection(direction);
+        var targetMovement = GetTargetMovement(oppositeDirection);
+        var obstacles = Physics2D.Linecast(currentPosition, currentPosition + targetMovement, obstaclesLayerMask);
+        if (!obstacles)
+        {
+            Undo(targetMovement);
+        }
+    }
+
+    private void Undo(Vector3 movement)
+    {
+        StartCoroutine(SmoothMove(movement));
     }
 
     IEnumerator SmoothMove(Vector3 targetMovement)
@@ -97,13 +137,13 @@ public class PlayerController2D : MonoBehaviour
         _ => throw new ArgumentOutOfRangeException(nameof(selectedDirection), $"Not expected direction value: {selectedDirection}"),
     };
     
-    private Vector3 GetTargetDirection() => selectedDirection switch
+    private Vector3 GetTargetMovement(Direction direction) => direction switch
     {
         Direction.Up => Vector3.up,
         Direction.Left => Vector3.left,
         Direction.Down => Vector3.down,
         Direction.Right => Vector3.right,
-        _ => throw new ArgumentOutOfRangeException(nameof(selectedDirection), $"Not expected direction value: {selectedDirection}"),
+        _ => throw new ArgumentOutOfRangeException(nameof(direction), $"Not expected direction value: {direction}"),
     };
 
     private void HandleInput()
@@ -120,20 +160,18 @@ public class PlayerController2D : MonoBehaviour
             }
         }
 
-        if (Input.GetKeyDown(KeyCode.RightArrow))
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            SetSelectedDirection(Direction.None);
+            undoLastMovement = true;
+        }
+        else if (Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            undoLastMovement = false;
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
         {
             SetSelectedDirection(GetNextAvailableDirection());
-        }
-        else if (Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            if (selectedDirection == Direction.None)
-            {
-                // UNDO last movement
-            }
-            else
-            {
-                SetSelectedDirection(Direction.None);
-            }
         }
     }
 
